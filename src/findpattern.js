faeca33ce2aaad11179d930755944daecc24c22e
weewoo14 <owenwdd01@gmail.com>
2025-08-14 22:11:31 -0400

    Optimized the counter array in findpattern.js

--- src/findpattern.js
+++ src/findpattern.js
@@ -11,34 +11,11 @@ function equalTwoArrays(arr1, arr2) {
 }
 
 function createFreqObj( str ) {
-  let counter = {
-    "a" : 0,
-    "b" : 0,
-    "c" : 0,
-    "d" : 0,
-    "e" : 0,
-    "f" : 0,
-    "g" : 0,
-    "h" : 0,
-    "i" : 0,
-    "j" : 0,
-    "k" : 0,
-    "l" : 0,
-    "m" : 0,
-    "n" : 0,
-    "o" : 0,
-    "p" : 0,
-    "q" : 0,
-    "r" : 0,
-    "s" : 0,
-    "t" : 0,
-    "u" : 0,
-    "v" : 0,
-    "w" : 0,
-    "x" : 0,
-    "y" : 0,
-    "z" : 0 
-  };
+  const startUnicodeValue = 97;
+  let counter = new Object();
+  for (let i = 0; i < 26; i++) {
+    counter[ String.fromCharCode( startUnicodeValue + i) ] = 0;
+  }
 
   for (let letter of str) {
     counter[ letter ] += 1;
